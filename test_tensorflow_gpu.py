1ad316ead96a34fda373ce90ca5b9ea106630c5a
Emre Can <emredjan@gmail.com>
2023-06-12 12:03:08 +0200

    Formatting

--- test_tensorflow_gpu.py
+++ test_tensorflow_gpu.py
@@ -4,11 +4,11 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from tensorflow.python.client import device_lib
 
-devices = {dev.device_type: dev.physical_device_desc for dev in device_lib.list_local_devices()}
+devices = {dev.device_type: dev.physical_device_desc for dev in device_lib.list_local_devices()}  # type: ignore
 
 if 'GPU' in devices.keys():
     print('Tensorflow: GPU available, will use the following device for calculation:')
-    print('\t' + devices.get('GPU'))
+    print(f"\t{devices.get('GPU')}")
 elif 'CPU' in devices.keys():
     print('Tensorflow: Only CPU available, no GPU calculation possible.')
 else:
